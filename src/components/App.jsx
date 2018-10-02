1868973e890b49b5ecb66cb73cd4b07d79e0dfc5
Hack Reactor Student <student@hackreactor.com>
2018-10-01 17:37:33 -0700

    Finished first two sets of tests

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,6 +16,11 @@ var App = () => (
   </div>
 );
 
+
+
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
+
+//render on index page
+ReactDOM.render(<App />, document.getElementById('app'));
